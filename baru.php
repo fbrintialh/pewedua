3ca35912ab3f6b25d81317d6c0c52db984fa1492
fbrinitialh <m.febri.nugroho@student.untan.ac.id>
2018-03-21 08:32:27 +0700

    Latihan 1, Halaman Pertama

---
+++ baru.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ini Halaman Pertama</title>
+</head>
+<body>
+	<h1>Ini Judul</h1>
+
+	<<?php 
+		echo "Inti Tulisan";
+	?>
+	<p>LoremIpsum</p>
+</body>
+</html>
